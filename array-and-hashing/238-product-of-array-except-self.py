6c90c56e41eba7c11498513c27b27fccc6eac162
alfred.chow <alfred.chow@bookmyshow.com>
2024-08-14 21:56:25 +0800

    20240814: 238 product of array except self - without divider

--- array-and-hashing/238-product-of-array-except-self.py
+++ array-and-hashing/238-product-of-array-except-self.py
@@ -33,3 +33,24 @@ class Solution:
                 res.append(int(total_product / n))
         
         return res
+    
+# Without using divider
+# Runtime: 269 ms, faster than 56.10% of Python3 online submissions for Product of Array Except Self.
+# Memory Usage: 25.7 MB, less than 47.43% of Python3 online submissions for Product of Array Except Self.
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        prefix = 1
+        postfix = 1
+        
+        # calculate and store prefix in res
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        # calculate and multiply postfix
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+            
+        return res           
