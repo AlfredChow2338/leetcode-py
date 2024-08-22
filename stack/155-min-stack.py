81978d5d0ec89cefbfb226264df0807b545017d2
alfred.chow <alfred.chow@bookmyshow.com>
2024-08-22 19:46:37 +0800

    20240822: 155 min stack - efficient

--- stack/155-min-stack.py
+++ stack/155-min-stack.py
@@ -1,3 +1,4 @@
+# Sorting
 # Runtime: 144 ms, faster than 11.89% of Python3 online submissions for Min Stack.
 # Memory Usage: 20.6 MB, less than 44.55% of Python3 online submissions for Min Stack.
 class MinStack:
@@ -19,3 +20,26 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minS[0]
+
+# Min Stack
+# Runtime: 50 ms, faster than 81.15% of Python3 online submissions for Min Stack.
+# Memory Usage: 20.6 MB, less than 44.55% of Python3 online submissions for Min Stack.
+class MinStack:
+    def __init__(self):
+        self.s = []
+        self.minS = []
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        minVal = min(val, self.minS[-1] if self.minS else val)
+        self.minS.append(minVal)
+
+    def pop(self) -> None:
+        self.s.pop()
+        self.minS.pop()
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.minS[-1]
